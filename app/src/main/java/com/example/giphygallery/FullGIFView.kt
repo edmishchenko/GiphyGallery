1545bee8e9e8bb98236c08f81ee589a6e2cda3c8
edmishchenko <eduardmishchenko86@gmail.com>
2021-07-15 23:06:14 +0300

    update 1.1

--- app/src/main/java/com/example/giphygallery/FullGIFView.kt
+++ app/src/main/java/com/example/giphygallery/FullGIFView.kt
@@ -1,11 +1,52 @@
 package com.example.giphygallery
 
+
+import android.annotation.SuppressLint
+import android.annotation.TargetApi
+import android.nfc.Tag
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.LinearLayout
+
 
 class FullGIFView : AppCompatActivity() {
+    lateinit var gif: WebView
+    lateinit var parentView: LinearLayout
+    private val YOUR_ANDROID_SDK_KEY = "w3YspOAlqfiCmFrza3Ebijj5KLPm1Ryx"
+    companion object{
+        const val gif_url = "gif"
+    }
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_full_gifview)
+
+        parentView = findViewById(R.id.parentView)
+        gif = findViewById(R.id.gif_view)
+
+        val result = intent.getStringExtra(gif_url)
+        gif.webViewClient = MyWebViewClient()
+        gif.settings.javaScriptEnabled = true
+        gif.loadUrl(result.toString());
+
     }
 }
+private class MyWebViewClient : WebViewClient() {
+    @TargetApi(Build.VERSION_CODES.N)
+    override fun shouldOverrideUrlLoading(view: WebView, request: WebResourceRequest): Boolean {
+        view.loadUrl(request.url.toString())
+        return true
+    }
+
+    // Для старых устройств
+    override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
+        view.loadUrl(url)
+        return true
+    }
+}
+
--- app/src/main/java/com/example/giphygallery/MainActivity.kt
+++ app/src/main/java/com/example/giphygallery/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.giphygallery
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.LinearLayout
@@ -12,6 +13,7 @@ import com.giphy.sdk.core.models.Media
 import com.giphy.sdk.ui.Giphy
 import com.giphy.sdk.ui.pagination.GPHContent
 import com.giphy.sdk.ui.views.GPHGridCallback
+import com.giphy.sdk.ui.views.GifView
 import com.giphy.sdk.ui.views.GiphyGridView
 
 
@@ -20,6 +22,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var btn_list: Button
     lateinit var parentView: LinearLayout
     lateinit var gifsGridView: GiphyGridView
+    lateinit var gifView: GifView
     private val YOUR_ANDROID_SDK_KEY = "w3YspOAlqfiCmFrza3Ebijj5KLPm1Ryx"
     private val DEFAULT_SPAN_COUNT = 3
     private var already_click = false
@@ -36,6 +39,7 @@ class MainActivity : AppCompatActivity() {
         Giphy.configure(this, YOUR_ANDROID_SDK_KEY)
 
        setGridViewParam(DEFAULT_SPAN_COUNT)
+
     }
 
     fun selectMedia(){
@@ -45,10 +49,15 @@ class MainActivity : AppCompatActivity() {
 
             override fun didSelectMedia(media: Media) {
                 val intent = Intent(this@MainActivity, FullGIFView::class.java)
-                startActivity(intent)
+
+                var med = media.embedUrl.toString()
                 Toast.makeText(this@MainActivity,
-                    "media selected: ${media.id}",
+                    "media selected: $med",
                     Toast.LENGTH_LONG).show()
+                var result = med.replace("https://giphy.com/embed/", "https://i.giphy.com/media/") + "/giphy.webp"
+
+                intent.putExtra(FullGIFView.gif_url, result)
+                startActivity(intent)
             }
         }
     }
@@ -66,10 +75,12 @@ class MainActivity : AppCompatActivity() {
         gifsGridView.content = GPHContent.trendingGifs
         selectMedia()
 
+
+
     }
 
     fun changeGridViewTypeGrid(view: View){
-        if (already_click == true) {
+        if (already_click) {
             parentView.removeView(gifsGridView)
             setGridViewParam(3)
         }
@@ -77,7 +88,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun changeGridViewTypeList(view: View){
-        if (already_click == false) {
+        if (!already_click) {
             parentView.removeView(gifsGridView)
             setGridViewParam(1)
         }
@@ -85,3 +96,5 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
+//https://i.giphy.com/media/cNwLEqnkZ99oL4oBKa/giphy.webp - need
+//https://giphy.com/embed/cNwLEqnkZ99oL4oBKa - what i take
