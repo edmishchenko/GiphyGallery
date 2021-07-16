4adf7b273510248f20964b3a66423185b2a1b14d
edmishchenko <eduardmishchenko86@gmail.com>
2021-07-16 15:45:17 +0300

    update 1.2

--- app/src/main/java/com/example/giphygallery/FullGIFView.kt
+++ app/src/main/java/com/example/giphygallery/FullGIFView.kt
@@ -3,39 +3,47 @@ package com.example.giphygallery
 
 import android.annotation.SuppressLint
 import android.annotation.TargetApi
-import android.nfc.Tag
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.view.ViewGroup
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
 
 
 class FullGIFView : AppCompatActivity() {
-    lateinit var gif: WebView
     lateinit var parentView: LinearLayout
-    private val YOUR_ANDROID_SDK_KEY = "w3YspOAlqfiCmFrza3Ebijj5KLPm1Ryx"
+    private val params: LinearLayout.LayoutParams = LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
     companion object{
         const val gif_url = "gif"
+        const val index_of_option = "index_of_option"
     }
-    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_full_gifview)
 
         parentView = findViewById(R.id.parentView)
-        gif = findViewById(R.id.gif_view)
+        // gif = findViewById(R.id.gif_view)
 
         val result = intent.getStringExtra(gif_url)
-        gif.webViewClient = MyWebViewClient()
-        gif.settings.javaScriptEnabled = true
-        gif.loadUrl(result.toString());
+        openInWebView(result)
+
 
     }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    fun openInWebView(url: String?){
+        val gif = WebView(this)
+        parentView.addView(gif, params)
+        gif.webViewClient = MyWebViewClient()
+        gif.settings.javaScriptEnabled = true
+        gif.loadUrl(url.toString());
+    }
+
 }
+
 private class MyWebViewClient : WebViewClient() {
     @TargetApi(Build.VERSION_CODES.N)
     override fun shouldOverrideUrlLoading(view: WebView, request: WebResourceRequest): Boolean {
@@ -43,10 +51,11 @@ private class MyWebViewClient : WebViewClient() {
         return true
     }
 
-    // Для старых устройств
+    // for old devices
     override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
         view.loadUrl(url)
         return true
     }
+
 }
 
--- app/src/main/java/com/example/giphygallery/MainActivity.kt
+++ app/src/main/java/com/example/giphygallery/MainActivity.kt
@@ -1,34 +1,87 @@
 package com.example.giphygallery
 
+import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
+import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
+import android.widget.EditText
 import android.widget.LinearLayout
-import android.widget.Toast
 import com.giphy.sdk.analytics.GiphyPingbacks.context
 import com.giphy.sdk.core.models.Media
+import com.giphy.sdk.core.models.enums.MediaType
 import com.giphy.sdk.ui.Giphy
 import com.giphy.sdk.ui.pagination.GPHContent
 import com.giphy.sdk.ui.views.GPHGridCallback
-import com.giphy.sdk.ui.views.GifView
 import com.giphy.sdk.ui.views.GiphyGridView
 
 
 class MainActivity : AppCompatActivity() {
     lateinit var btn_grid: Button
     lateinit var btn_list: Button
+    lateinit var btn_search: Button
+    lateinit var searchInput: EditText
     lateinit var parentView: LinearLayout
+    lateinit var ll_search: LinearLayout
     lateinit var gifsGridView: GiphyGridView
-    lateinit var gifView: GifView
     private val YOUR_ANDROID_SDK_KEY = "w3YspOAlqfiCmFrza3Ebijj5KLPm1Ryx"
     private val DEFAULT_SPAN_COUNT = 3
     private var already_click = false
+    private var click = false
     private val Tag = "myLogs"
+    private val ll_params_true: LinearLayout.LayoutParams = LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+    private val ll_params_false : LinearLayout.LayoutParams = LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 0)
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_fullgifview, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
 
+    @SuppressLint("ShowToast")
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.ic_search ->  {
+                if(!click) {
+                    showSearchLine()
+                    click = true
+                }
+                else {
+                    hideSearchLine()
+                    click = false
+                }
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    fun showSearchLine() {
+        ll_search.layoutParams = ll_params_true
+    }
+
+    fun hideSearchLine(){
+        ll_search.layoutParams = ll_params_false
+    }
+
+    private fun performCustomSearch() {
+        if (searchInput.text.isNullOrEmpty())
+            gifsGridView.content = GPHContent.trendingGifs
+        else
+            gifsGridView.content = GPHContent.searchQuery(searchInput.text.toString(), MediaType.gif)
+    }
+
+    fun dismissKeyboard() {
+        val imm = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(searchInput.windowToken, 0)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -36,10 +89,37 @@ class MainActivity : AppCompatActivity() {
         btn_grid = findViewById(R.id.btn_grid)
         btn_list = findViewById(R.id.btn_list)
         parentView = findViewById(R.id.parentView)
+        ll_search = findViewById(R.id.ll_search)
+        searchInput = findViewById(R.id.search_input)
+        btn_search = findViewById(R.id.btn_search)
         Giphy.configure(this, YOUR_ANDROID_SDK_KEY)
 
        setGridViewParam(DEFAULT_SPAN_COUNT)
 
+        btn_search.setOnClickListener{
+            dismissKeyboard()
+            performCustomSearch()
+        }
+
+        searchInput.setOnEditorActionListener { v, actionId, event ->
+            if (actionId == EditorInfo.IME_ACTION_SEARCH || actionId == EditorInfo.IME_NULL || actionId == EditorInfo.IME_ACTION_GO){
+                dismissKeyboard()
+                performCustomSearch()
+                return@setOnEditorActionListener true
+            }
+            false
+        }
+
+        searchInput.addTextChangedListener(object : TextWatcher{
+            override fun afterTextChanged(s: Editable?) {
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) = Unit
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                performCustomSearch()
+            }
+        })
     }
 
     fun selectMedia(){
@@ -48,18 +128,23 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun didSelectMedia(media: Media) {
-                val intent = Intent(this@MainActivity, FullGIFView::class.java)
-
-                var med = media.embedUrl.toString()
-                Toast.makeText(this@MainActivity,
-                    "media selected: $med",
-                    Toast.LENGTH_LONG).show()
-                var result = med.replace("https://giphy.com/embed/", "https://i.giphy.com/media/") + "/giphy.webp"
-
-                intent.putExtra(FullGIFView.gif_url, result)
-                startActivity(intent)
+                callFullGIFView(convertGIFurl(media))
             }
+
         }
+    }
+
+    fun convertGIFurl(media: Media): String {
+        var med = media.embedUrl.toString()
+        var result = med.replace("https://giphy.com/embed/", "https://i.giphy.com/media/") + "/giphy.webp"
+        return result
+    }
+
+
+    fun callFullGIFView(result: String?){
+        val intent = Intent(this@MainActivity, FullGIFView::class.java)
+        intent.putExtra(FullGIFView.gif_url, result)
+        startActivity(intent)
     }
     fun setGridViewParam(span_count: Int){
 
@@ -74,9 +159,6 @@ class MainActivity : AppCompatActivity() {
         parentView.addView(gifsGridView)
         gifsGridView.content = GPHContent.trendingGifs
         selectMedia()
-
-
-
     }
 
     fun changeGridViewTypeGrid(view: View){
@@ -86,8 +168,7 @@ class MainActivity : AppCompatActivity() {
         }
         already_click = false
     }
-
-    fun changeGridViewTypeList(view: View){
+    fun changeGridViewTypeList(view: View) {
         if (!already_click) {
             parentView.removeView(gifsGridView)
             setGridViewParam(1)
@@ -95,6 +176,3 @@ class MainActivity : AppCompatActivity() {
         already_click = true
     }
 }
-
-//https://i.giphy.com/media/cNwLEqnkZ99oL4oBKa/giphy.webp - need
-//https://giphy.com/embed/cNwLEqnkZ99oL4oBKa - what i take
